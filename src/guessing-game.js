bcdf7fc6d55d27939fdf613c549fd5d1769bd647
Jimmba <gimvit@gmail.com>
2019-10-11 00:27:22 +0300

    feat: task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    //Алгоритм двоичного поиска, см файл md
+    constructor() {
+        let min, max, value;
+    }
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
     }
 
     guess() {
-
+        this.value = Math.round((this.min+this.max)/2);
+        return this.value;
     }
 
     lower() {
-
+        this.max=this.value;
     }
 
     greater() {
-
+        this.min=this.value;
     }
 }
 
